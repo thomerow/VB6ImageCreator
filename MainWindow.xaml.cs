f5d3587c02144952b20497053dcc43dba6a667da
Thorsten Kummerow <thorsten.kummerow@googlemail.com>
2013-02-21 12:10:14 +0100

    Added color sliders

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,5 +23,92 @@ namespace VB6ImageCreator
       {
          InitializeComponent();
       }
+
+      private void SliderRed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+      {
+         // Green rectangle
+         var lgb = _rectGreen.Fill as LinearGradientBrush;
+         if (lgb == null) return;
+
+         var stop0 = lgb.GradientStops[0];
+         var stop1 = lgb.GradientStops[1];
+
+         stop0.Color = new Color { A = 0xFF, R = (byte) e.NewValue, G = stop0.Color.G, B = stop0.Color.B };
+         stop1.Color = new Color { A = 0xFF, R = (byte) e.NewValue, G = stop1.Color.G, B = stop1.Color.B };
+
+         // Blue rectangle
+         lgb = _rectBlue.Fill as LinearGradientBrush;
+         if (lgb == null) return;
+
+         stop0 = lgb.GradientStops[0];
+         stop1 = lgb.GradientStops[1];
+
+         stop0.Color = new Color { A = 0xFF, R = (byte) e.NewValue, G = stop0.Color.G, B = stop0.Color.B };
+         stop1.Color = new Color { A = 0xFF, R = (byte) e.NewValue, G = stop1.Color.G, B = stop1.Color.B };
+
+         _txtRed.Text = ((byte) e.NewValue).ToString();
+
+         UpdateSelectedColorRect();
+      }
+
+      private void SliderGreen_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+      {
+         // Red rectangle
+         var lgb = _rectRed.Fill as LinearGradientBrush;
+         if (lgb == null) return;
+
+         var stop0 = lgb.GradientStops[0];
+         var stop1 = lgb.GradientStops[1];
+
+         stop0.Color = new Color { A = 0xFF, R = stop0.Color.R, G = (byte) e.NewValue, B = stop0.Color.B };
+         stop1.Color = new Color { A = 0xFF, R = stop1.Color.R, G = (byte) e.NewValue, B = stop1.Color.B };
+
+         // Blue rectangle
+         lgb = _rectBlue.Fill as LinearGradientBrush;
+         if (lgb == null) return;
+
+         stop0 = lgb.GradientStops[0];
+         stop1 = lgb.GradientStops[1];
+
+         stop0.Color = new Color { A = 0xFF, R = stop0.Color.R, G = (byte) e.NewValue, B = stop0.Color.B };
+         stop1.Color = new Color { A = 0xFF, R = stop1.Color.R, G = (byte) e.NewValue, B = stop1.Color.B };
+
+         _txtGreen.Text = ((byte) e.NewValue).ToString();
+
+         UpdateSelectedColorRect();
+      }
+
+      private void SliderBlue_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+      {
+         // Red rectangle
+         var lgb = _rectRed.Fill as LinearGradientBrush;
+         if (lgb == null) return;
+
+         var stop0 = lgb.GradientStops[0];
+         var stop1 = lgb.GradientStops[1];
+
+         stop0.Color = new Color { A = 0xFF, R = stop0.Color.R, G = stop0.Color.G, B = (byte) e.NewValue };
+         stop1.Color = new Color { A = 0xFF, R = stop1.Color.R, G = stop1.Color.G, B = (byte) e.NewValue };
+
+         // Green rectangle
+         lgb = _rectGreen.Fill as LinearGradientBrush;
+         if (lgb == null) return;
+
+         stop0 = lgb.GradientStops[0];
+         stop1 = lgb.GradientStops[1];
+
+         stop0.Color = new Color { A = 0xFF, R = stop0.Color.R, G = stop0.Color.G, B = (byte) e.NewValue };
+         stop1.Color = new Color { A = 0xFF, R = stop1.Color.R, G = stop1.Color.G, B = (byte) e.NewValue };
+
+         _txtBlue.Text = ((byte) e.NewValue).ToString();
+
+         UpdateSelectedColorRect();
+      }
+
+      private void UpdateSelectedColorRect()
+      {
+         _rectColor.Fill 
+            = new SolidColorBrush(new Color { A = 0xFF, R = (byte) _sliderRed.Value, G = (byte) _sliderGreen.Value, B = (byte) _sliderBlue.Value });
+      }
    }
 }
